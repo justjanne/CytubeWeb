32a47709f8845245220ce900d8ee784770d53c81
Ron George <rgeorge98@Outlook.com>
2016-02-28 17:07:36 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -217,7 +217,7 @@ if (typeof (CUSTOM) === "undefined") CUSTOM = {
   var externalScripts = [
     {
       "type": "css",
-      "url": path + "custom.css",
+      "url": "https://rawgit.com/lb77/CytubeWeb/master/custom.css",
       "callback": []
     },
     {
